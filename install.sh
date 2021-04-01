70337a35e0a492040e1b3e2d2640c48562a97709
SanzzGanzs <81704601+SanzzGanzs@users.noreply.github.com>
2021-04-01 15:56:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ apt-get install wget
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install --dev
+npm install brainly-scraper
 npm audit fix
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
